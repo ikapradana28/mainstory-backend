5ec2f9cfbe54eb890465a4c16273a436514c4cc0
Ika Pradana Cintaningtiyas <ikapradanaac@gmail.com>
2026-04-19 16:31:24 +0700

    update changes

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running");
-});
-
 const app = express();
 const prisma = new PrismaClient();
 
